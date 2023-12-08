4ca6fbd0ae06694460324eb106c0d15949c90baa
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-12-09 00:55:42 +0500

    Add project files.

---
+++ Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+
+            queue.Enqueue("Birinchi element");
+            queue.Enqueue("Ikkinchi element");
+            queue.Enqueue("Uchinchi element");
+            queue.Enqueue("To'rtinchi element");
+
+            var element = queue.Peek();  //Birinchi element
+            Console.WriteLine(element);
+
+            Console.WriteLine(new string('-', 20));
+
+            while(queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+
+            stack.Push("Birinchi element");
+            stack.Push("Ikkinchi element");
+            stack.Push("Uchinchi element");
+            stack.Push("To'rtinchi element");
+            var element = stack.Peek();  //To'rtinchi element
+            Console.WriteLine(element);
+
+            Console.WriteLine(new string('-', 20));
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
